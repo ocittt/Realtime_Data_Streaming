e39ffceb2965650805b8b048942991461cd1e4a5
Imtiyaz <63872669+ocittt@users.noreply.github.com>
2024-07-22 11:28:17 +0700

    fixed issue, dags finally pushed to kafka

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,15 +1,15 @@
 from datetime import datetime
-import json
 from airflow import DAG 
 from airflow.operators.python import PythonOperator
 
 default_args = {
     'owner': 'airocit',
-    'start_date': datetime(2024, 6, 26, 17, 00)
+    'start_date': datetime(2024, 6, 28, 10, 00)
 }
 
 def get_data():
     import requests
+    
     res = requests.get("https://randomuser.me/api/")
     res = res.json()
     res = res['results'][0]
@@ -22,7 +22,8 @@ def format_data(res):
     data['first_name'] = res['name']['first']
     data['last_name'] = res['name']['last']
     data['gender'] = res['gender']
-    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, {location['city']}, {location['state']}, {location['country']}"
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
+                      f"{location['city']}, {location['state']}, {location['country']}"
     data['post_code'] = location['postcode']
     data['email'] = res['email']
     data['dob'] = res['dob']['date']
@@ -37,6 +38,10 @@ def stream_data():
     from kafka import KafkaProducer
     import time
     import logging
+
+
+    # res = get_data()
+    # res = format_data(res)
 
     producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
     curr_time = time.time()
@@ -62,3 +67,5 @@ with DAG('user_automation',
         task_id='stream_data_from_api',
         python_callable=stream_data
     )
+
+# stream_data()
